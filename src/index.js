dabfb7f4a042e8c8ac14d0af473dc72be7dfb4cf
atroopa <atroopa@gmail.com>
2023-04-23 19:45:06 +0330

    create counter component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,46 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {createStore, combineReducers} from "redux"
 
+//const store = createStore()
+
+// const addCounter = number => {
+//   return {
+//     type: "ADD_COUNTER",
+//     payload: number
+//   }
+// }
+
+// const decresCounter = number => {
+//   return {
+//     type:"DECRES_COUNTER",
+//     payload: number
+//   }
+// }
+
+// const counterApp = (state, action) => {
+
+//   switch (action.type) {
+//     case "ADD_COUNTER":
+//       return state ++;
+//     case "DECRES_COUNTER":
+//       return state --;  
+  
+//     default:
+//       return state;
+//   }
+
+// }
+
+
+// const final_reducers = combineReducers({
+//   counterApp,
+// })
+
+const initialState = {
+  count:0,
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
