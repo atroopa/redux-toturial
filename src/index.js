7569714c4ba8989e4977d2ca574dc255e707d676
atroopa <atroopa@gmail.com>
2023-04-23 20:05:03 +0330

    reducer coded

--- src/index.js
+++ src/index.js
@@ -5,44 +5,26 @@ import App from './App';
 import {createStore, combineReducers} from "redux"
 import Counter from './components/Counter';
 
-//const store = createStore()
 
-// const addCounter = number => {
-//   return {
-//     type: "ADD_COUNTER",
-//     payload: number
-//   }
-// }
-
-// const decresCounter = number => {
-//   return {
-//     type:"DECRES_COUNTER",
-//     payload: number
-//   }
-// }
-
-// const counterApp = (state, action) => {
-
-//   switch (action.type) {
-//     case "ADD_COUNTER":
-//       return state ++;
-//     case "DECRES_COUNTER":
-//       return state --;  
-  
-//     default:
-//       return state;
-//   }
-
-// }
-
-
-// const final_reducers = combineReducers({
-//   counterApp,
-// })
 
 const initialState = {
   count:0,
 }
+
+const reducer = (state= initialState, action) => {
+  switch (action.type) {
+    
+    case "INCREMENT_COUNTER":
+      return {count: state.count + action.payload};
+
+    case "DECREMENT_COUNTER":
+      return{count: state.count - action.payload};
+  
+    default:
+      return state;
+  }
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
